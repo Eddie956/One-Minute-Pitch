80f24d3010d6b37b63b331ac8496c13e9619fc99
Eddie956 <mutugieddie3@gmail.com>
2018-06-25 07:05:44 +0300

    this will commit all the files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://murungi:murungi1@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://a:mango@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/images'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -9,7 +9,6 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    # CRSF_SECRET_KEY =os.environ.get("SECRET_KEY")
     CRSF_SECRET_KEY = 'x17M\xeacL>|i\xfa\x91\xcey,\x159\x01\xca2\x8a\x8dl\xd9'
 
 class ProdConfig(Config):
@@ -17,11 +16,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://murungi:murungi1@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://a:mango@localhost/pitch'
     DEBUG = True
 
 class TestConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://murungi:murungi1@localhost/pitch_test'
     pass
 config_options = {
     'development': DevConfig,
